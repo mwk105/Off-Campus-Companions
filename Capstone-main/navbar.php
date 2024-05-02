40087c04417806cd20fbc0e15951d3463f9e95b9
alexpinzon1 <144372847+alexpinzon1@users.noreply.github.com>
2024-05-01 22:10:11 -0400

    Add files via upload

--- Capstone-main/navbar.php
+++ Capstone-main/navbar.php
@@ -1,11 +1,11 @@
 <nav class="menu">
     <ul>
-        <li><a href="landing_page.html">Home</a></li>
-        <li><a href="profile.php">Profile</a></li>
-        <li><a href="reviews.php">Reviews + Ratings</a></li>
-        <li><a href="faq.php">FAQ</a></li>
-        <li><a href="user_search.php">User Search</a></li>
-        <li><a href="forum.php">User Forum</a></li>
-        <li><a href="index.html">Logout</a></li>
+    <li><a href="landing_page.html">Home</a></li>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="user_search.php">User Search</a></li>
+            <li><a href="forum.html">User Forum</a></li>
+            <li><a href="reviews.php">Reviews + Ratings</a></li>
+            <li><a href="faq.php">FAQ</a></li>   
+            <li><a href="index.html">Logout</a></li>
     </ul>
 </nav>
--- Capstone-main/post_details.php
+++ Capstone-main/post_details.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -114,12 +113,12 @@
     <nav class="menu">
         <ul>
         <li><a href="landing_page.html">Home</a></li>
-                <li><a href="profile.php">Profile</a></li>
-                <li><a href="forum.php">User Forum</a></li>
-                <li><a href="user_search.php">User Search</a></li>    
-                <li><a href="reviews.php">Reviews + Ratings</a></li>
-                <li><a href="faq.php">FAQ</a></li>
-                <li><a href="index.html">Logout</a></li>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="user_search.php">User Search</a></li>
+            <li><a href="forum.html">User Forum</a></li>
+            <li><a href="reviews.php">Reviews + Ratings</a></li>
+            <li><a href="faq.php">FAQ</a></li>   
+            <li><a href="index.html">Logout</a></li>
         </ul>
     </nav>
 
